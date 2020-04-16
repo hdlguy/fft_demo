ed685823e7d0f6eb8583b4a495edc9effcda9e0b
pedro <hdlguy@hotmail.com>
2020-04-16 12:52:19 -0400

    working on fft based correlation - it's tough

--- conv.cpp
+++ conv.cpp
@@ -10,15 +10,8 @@ using std::vector;
 
 void window(vector<double> *vec) 
 {
-    // Hamming.
-    //const double a = 0.54;
-    //const double b = 1.0 - a;
-    //vector<double>::size_type n = vec->size();
-    //for(int i=0; i<n; i++) (*vec)[i] *= a - b*cos(2.0*M_PI*i/(n-1));
-
-    // Hanning
     vector<double>::size_type n = vec->size();
-    for(int i=0; i<n; i++) (*vec)[i] *= 0.5*(1.0-cos(2.0*M_PI*i/(n-1)));
+    for(int i=0; i<n; i++) (*vec)[i] *= 0.5*(1.0-cos(2.0*M_PI*i/(n-1))); // Hanning
 }
 
 
@@ -27,6 +20,7 @@ int main(int argc, char **argv) {
     const int Nt    =  32;
     const int Off   =   5;
     const int Nfft  =  (int)pow(2.0, ceil(log2(2.0*Nt-1))); // 2^(ceil(log2(2*Nt-1)));
+    cout << "Nfft = " << Nfft << "\n";
 
 
     // random signal
@@ -37,36 +31,51 @@ int main(int argc, char **argv) {
         s_imag[i] = ((double)rand())/((double)RAND_MAX) - 0.5;
     }
 
-    // take a portion of the signal to be the convolution template.
+    // take the reverse order complex conjugate of a portion of the signal to be the convolution template.
     vector<double> h_real(Nt);
     vector<double> h_imag(Nt);
     for(int i=0; i<Nt; i++) {
-        h_real[i] = s_real[i+Off];
-        h_imag[i] = s_imag[i+Off];
+        h_imag[i] = -s_imag[Nt-1-i+Off];
+        h_real[i] = +s_real[Nt-1-i+Off];
     }
 
-/*
-    // zero pad the vectors.
-    vector<double> real_vec_pad(Nfft, 0.0);
-    vector<double> imag_vec_pad(Nfft, 0.0);
-    for (int i=0; i<N; i++) real_vec_pad[i] = real_vec[i];
-    for (int i=0; i<N; i++) imag_vec_pad[i] = imag_vec[i];
+    vector<double> h_pad_imag(Nfft, 0.0);
+    vector<double> h_pad_real(Nfft, 0.0);
+    vector<double> s_pad_imag(Nfft, 0.0);
+    vector<double> s_pad_real(Nfft, 0.0);
+    for(int i=0; i<Nt; i++) {
+        h_pad_imag[i] = h_imag[i];
+        h_pad_real[i] = h_real[i];
+        s_pad_imag[i] = s_imag[i];
+        s_pad_real[i] = s_real[i];
+    }
+    
+    vector<double> H_imag(Nfft); H_imag = h_pad_imag;
+    vector<double> H_real(Nfft); H_real = h_pad_real;
+    Fft::transformRadix2(H_real, H_imag);
+
+    vector<double> S_imag(Nfft); S_imag = s_pad_imag;
+    vector<double> S_real(Nfft); S_real = s_pad_real;
+    Fft::transformRadix2(S_real, S_imag);
+
+    vector<double> Y_imag(Nfft);
+    vector<double> Y_real(Nfft);
+    for(int i=0; i<Nfft; i++) {
+        Y_imag[i] = (H_real[i]*S_real[i] - H_imag[i]*S_imag[i]);
+        Y_real[i] = (H_real[i]*S_imag[i] + H_imag[i]*S_real[i]);
+    }
+
+    vector<double> y_imag(Nfft); y_imag = Y_imag;
+    vector<double> y_real(Nfft); y_real = Y_real;
+    Fft::transformRadix2(y_imag, y_real);  // real <-> imag swapped to implement inverse fft (without scaling)
 
     // plot time domain data
-    FILE* gnuplotPipe = popen ("gnuplot -persistent", "w");
+    FILE* gnuplotPipe = popen ("gnuplot --persist", "w");
     fprintf(gnuplotPipe, "plot '-' ps 0.1 lt 6\n");
-    for (int i=0; i<N*P; i++) fprintf(gnuplotPipe, "%lf\n", real_vec_pad[i]);
+    for (int i=0; i<Nfft; i++) fprintf(gnuplotPipe, "%lf\n", h_pad_real[i]);
     fprintf(gnuplotPipe, "e");
     pclose(gnuplotPipe);
 
-
-    // Plot frequency domain data in dB
-    gnuplotPipe = popen ("gnuplot -persistent", "w");
-    fprintf(gnuplotPipe, "plot '-' ps 0.1 lt 6\n");
-    for (int i=0; i<N*P; i++) fprintf(gnuplotPipe, "%lf\n", 10*log10(powvec[i]));
-    fprintf(gnuplotPipe, "e");
-    pclose(gnuplotPipe);
-*/
 
     return EXIT_SUCCESS;
 }
