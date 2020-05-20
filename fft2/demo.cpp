e63de4d7157b1f4e9a6bb9687c0cda67d39f9ce3
pedro <hdlguy@hotmail.com>
2020-05-20 11:13:38 -0400

    fft with vectors replaced by regular C arrays.

---
+++ fft2/demo.cpp
@@ -0,0 +1,70 @@
+#include <cmath>
+#include <cstdlib>
+#include <iostream>
+#include <vector>
+#include <array>
+#include "fft.hpp"
+
+using std::cout;
+using std::endl;
+//using std::vector;
+using std::array;
+
+void window(double *vec, int n) {
+    for(int i=0; i<n; i++) vec[i] *= 0.5*(1.0-cos(2.0*M_PI*i/(n-1))); // Hanning
+}
+
+
+int main(int argc, char **argv) {
+    const int N  = 4096;
+    const int P = 16;
+    const double Fs = 327.68e6;
+    const double Fc = (+500.1/N)*Fs;
+    const double Wc = 2*M_PI*Fc/Fs;
+    const int B = 10;
+    const double A = pow(2.0,(B-1))-1.0;
+    cout << "A = " << A << "\n";
+
+    double real_vec[N];
+    double imag_vec[N];
+
+    // synthesize quantized sinusoids
+    for(int i=0; i<N; i++) {
+        real_vec[i] = round(A*cos(Wc*i));
+        imag_vec[i] = round(A*sin(Wc*i));
+    }
+
+    // window the data for spectral analysis
+    window(real_vec, N);
+    window(imag_vec, N);
+
+    // zero pad the vectors.
+    double real_vec_pad[N*P], imag_vec_pad[N*P]; 
+    for (int i=0; i<N*P; i++) real_vec_pad[i] = 0.0;
+    for (int i=0; i<N*P; i++) imag_vec_pad[i] = 0.0;
+    for (int i=0; i<N;   i++) real_vec_pad[i] = real_vec[i];
+    for (int i=0; i<N;   i++) imag_vec_pad[i] = imag_vec[i];
+
+    // plot time domain data
+    FILE* gnuplotPipe = popen ("gnuplot -persistent", "w");
+    fprintf(gnuplotPipe, "plot '-' ps 0.1 lt 6\n");
+    for (int i=0; i<N*P; i++) fprintf(gnuplotPipe, "%lf\n", real_vec_pad[i]);
+    fprintf(gnuplotPipe, "e");
+    pclose(gnuplotPipe);
+
+    // compute and convert to power
+    Fft::transform(real_vec_pad, imag_vec_pad, N*P);
+    double powvec[N*P];
+    for(int i=0; i<N*P; i++) powvec[i] = pow(real_vec_pad[i]/A,2.0) + pow(imag_vec_pad[i]/A,2.0);
+
+    // Plot frequency domain data in dB
+    gnuplotPipe = popen ("gnuplot -persistent", "w");
+    fprintf(gnuplotPipe, "plot '-' ps 0.1 lt 6\n");
+    for (int i=0; i<N*P; i++) fprintf(gnuplotPipe, "%lf\n", 10*log10(powvec[i]));
+    //for (int i=0; i<N*P; i++) fprintf(gnuplotPipe, "%lf\n", powvec[i]);
+    fprintf(gnuplotPipe, "e");
+    pclose(gnuplotPipe);
+
+    return EXIT_SUCCESS;
+}
+
---
+++ fft2/fft.cpp
@@ -0,0 +1,77 @@
+#include <cmath>
+#include <cstdint>
+//#include <vector>
+#include "fft.hpp"
+
+using std::size_t;
+//using std::vector;
+
+
+static size_t reverseBits(size_t x, unsigned int n); // Private function prototype
+
+void Fft::transform(double *real, double *imag, int n) {
+    transformRadix2(real, imag, n);
+}
+
+void Fft::transformRadix2(double *real, double *imag, int n) {
+	// Compute levels = floor(log2(n))
+	unsigned int levels;
+	{
+		size_t temp = n;
+		levels = 0;
+		while (temp > 1) {
+			levels++;
+			temp >>= 1;
+		}
+		if (1u << levels != n)
+			throw "Length is not a power of 2";
+	}
+	
+	// Trignometric tables
+	double cosTable[n/2];
+	double sinTable[n/2];
+	for (size_t i = 0; i < n / 2; i++) {
+		cosTable[i] = cos(2 * M_PI * i / n);
+		sinTable[i] = sin(2 * M_PI * i / n);
+	}
+	
+	// Bit-reversed addressing permutation
+	for (size_t i = 0; i < n; i++) {
+		size_t j = reverseBits(i, levels);
+		if (j > i) {
+			double temp = real[i];
+			real[i] = real[j];
+			real[j] = temp;
+			temp = imag[i];
+			imag[i] = imag[j];
+			imag[j] = temp;
+		}
+	}
+	
+	// Cooley-Tukey decimation-in-time radix-2 FFT
+	for (size_t size = 2; size <= n; size *= 2) {
+		size_t halfsize = size / 2;
+		size_t tablestep = n / size;
+		for (size_t i = 0; i < n; i += size) {
+			for (size_t j = i, k = 0; j < i + halfsize; j++, k += tablestep) {
+				double tpre =  real[j+halfsize] * cosTable[k] + imag[j+halfsize] * sinTable[k];
+				double tpim = -real[j+halfsize] * sinTable[k] + imag[j+halfsize] * cosTable[k];
+				real[j + halfsize] = real[j] - tpre;
+				imag[j + halfsize] = imag[j] - tpim;
+				real[j] += tpre;
+				imag[j] += tpim;
+			}
+		}
+		if (size == n)  // Prevent overflow in 'size *= 2'
+			break;
+	}
+}
+
+
+static size_t reverseBits(size_t x, unsigned int n) {
+	size_t result = 0;
+	unsigned int i;
+	for (i = 0; i < n; i++, x >>= 1)
+		result = (result << 1) | (x & 1);
+	return result;
+}
